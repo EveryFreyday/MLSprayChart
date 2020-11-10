8e2ca4f5e926009d8b17cc6d7738b3ef5b23d5f2
Matt Frey <matttfrey@gmail.com>
2020-11-10 14:26:47 -0500

    Commenting general idea

---
+++ Preprocessing.R
@@ -0,0 +1,23 @@
+# Matt Frey
+# DATS 6202 - Machine learning project
+# Use ML to position MLB defenses
+
+# Main idea
+#
+# -Develop machine learning based analysis that outputs spray charts by players
+
+# Goals
+# 
+# -Use random forest or dplyr data science package
+# -Shiny App to display
+
+# The libraries I will use
+library(dplyr)
+
+# This should have a majority of the data I need, as well as plotting functions (baseball fields rather than ugly cartesian graphs.)
+library(baseballr)
+
+#general graphing 
+library(ggplot2)
+
+
