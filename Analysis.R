3d20ba8695613fa50cbb3a474103bbb33e1fb7bc
Matt Frey <matttfrey@gmail.com>
2020-12-07 23:26:08 -0500

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -10,6 +10,7 @@ library(caTools)
 library(corrgram)
 library(readr)
 library(MASS)
+library(baseballr)
 
 # Read in data
 rawclean <- read_csv("~/MLSprayChart/rawclean.csv")
@@ -46,4 +47,29 @@ sampleSplit <- sample.split(Y=keep$hc_x, SplitRatio=0.7)
 trainSet <- subset(x=keep, sampleSplit==TRUE)
 testSet <- subset(x=keep, sampleSplit==FALSE)
 
-model <- lm(keep$hc_x ~. , data=trainSet)
+model <- lm(hc_x ~. , data=trainSet)
+
+model1 <- lm(hc_y ~. , data=trainSet)
+
+summary(model)
+summary(model1)
+
+modelResiduals <- as.data.frame(residuals(model)) 
+
+modelResiduals1 <- as.data.frame(residuals(model1)) 
+
+prediction <- cbind(modelResiduals, modelResiduals1)
+
+ggspraychart(
+  prediction,
+  x_value = "residuals(model1)",
+  y_value = "residuals(model)",
+  fill_value = NULL,
+  fill_palette = NULL,
+  fill_legend_title = NULL,
+  density = FALSE,
+  bin_size = 15,
+  point_alpha = 0.75,
+  point_size = 2,
+  frame = NULL
+) + ggtitle("Joey Votto Prediction")
